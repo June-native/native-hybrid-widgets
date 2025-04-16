840f2d659984c7f45e15b7ae64c7a8680bfc5071
June <june@native.org>
2025-04-16 15:42:27 +0800

    fix: patched ver NFPMs, update base addresses

--- packages/native-ammv3-widgets/src/widgets/PoolWidget/AMMV3/sdks/sdk-core/addresses.ts
+++ packages/native-ammv3-widgets/src/widgets/PoolWidget/AMMV3/sdks/sdk-core/addresses.ts
@@ -38,7 +38,8 @@ const MAINNET_ADDRESSES: ChainAddresses = {
   v3CoreFactoryAddress: '0xB4cCd44D3bABe05DfC70b39944796d281AE905EE',
   NativeV3PoolDeployer: '0x93c69485C572aca0467A395aF6f5c7FE49FCA83F',
   nonfungiblePositionManagerAddress:
-    '0x6aab72787F7E9EE48c45fc97B0FD0F7B25F80cB9',
+    // '0x6aab72787F7E9EE48c45fc97B0FD0F7B25F80cB9',
+    '0x7f5Bd5688D1cE33b4BA3B3CA5901E69188ef994d', // patched slot0.protocolFee datatype
   theGraphUrl:
     'https://gateway.thegraph.com/api/subgraphs/id/6hXjt23gepeW56vJJTfQim1kVdm1bjEvqVZfu3tpGny6',
   poolInitCodeHash:
@@ -49,7 +50,8 @@ const ARBITRUM_ONE_ADDRESSES: ChainAddresses = {
   v3CoreFactoryAddress: '0xB4cCd44D3bABe05DfC70b39944796d281AE905EE',
   NativeV3PoolDeployer: '0x93c69485C572aca0467A395aF6f5c7FE49FCA83F',
   nonfungiblePositionManagerAddress:
-    '0x6aab72787F7E9EE48c45fc97B0FD0F7B25F80cB9',
+    // '0x6aab72787F7E9EE48c45fc97B0FD0F7B25F80cB9',
+    '0xe86e803810555dB05d2c85b6aa9Ea36Bd25FD117', // patched slot0.protocolFee datatype
   theGraphUrl:
     'https://gateway.thegraph.com/api/subgraphs/id/m7jy1LL9aihKD7arZXwgkDHRoy1xp43LoVtPDuxVcjn',
   poolInitCodeHash:
@@ -60,7 +62,7 @@ const ARBITRUM_ONE_ADDRESSES: ChainAddresses = {
 const SEPOLIA_ADDRESSES: ChainAddresses = {
   v3CoreFactoryAddress: '0x3d2A7Bac4E8439ABe86B58324695e921a5FC0987',
   nonfungiblePositionManagerAddress:
-    '0x483E5c0f309577f79b0a19cE65E332DD388aD7A8',
+    '0x483E5c0f309577f79b0a19cE65E332DD388aD7A8', // broken if protocolFee > 0
   poolInitCodeHash:
     '0x4509fa1e2d1989ac1632a56fe87c53e8d1e9d05847694e00f62b23e28cec98c4',
 };
@@ -69,7 +71,7 @@ const ARBITRUM_SEPOLIA_ADDRESSES: ChainAddresses = {
   v3CoreFactoryAddress: '0x241Fb4e9F9518F9324fb8Bf042EaB5E564e881FB',
   NativeV3PoolDeployer: '0xca99D5E976d45Aa7bB142F405a5afc050dDaA948',
   nonfungiblePositionManagerAddress:
-    '0xF582f3D26103f91D9828613453d98065aea4C49a',
+    '0xF582f3D26103f91D9828613453d98065aea4C49a', // broken if protocolFee > 0
   poolInitCodeHash:
     '0x6f4fcfea116711b167b87469b22333669e4ceb8f39b24bef28be93f45af8b70c',
 };
@@ -78,7 +80,8 @@ const BSC_ADDRESSES: ChainAddresses = {
   v3CoreFactoryAddress: '0xB4cCd44D3bABe05DfC70b39944796d281AE905EE',
   NativeV3PoolDeployer: '0x93c69485C572aca0467A395aF6f5c7FE49FCA83F',
   nonfungiblePositionManagerAddress:
-    '0x6aab72787F7E9EE48c45fc97B0FD0F7B25F80cB9',
+    // '0x6aab72787F7E9EE48c45fc97B0FD0F7B25F80cB9',
+    '0x7f5Bd5688D1cE33b4BA3B3CA5901E69188ef994d', // patched slot0.protocolFee datatype
   theGraphUrl:
     'https://gateway.thegraph.com/api/subgraphs/id/8171zkXXXz6pv98vpRwnTUueKCU2eHuu9BMxT8UR8KbB',
   poolInitCodeHash:
@@ -86,21 +89,22 @@ const BSC_ADDRESSES: ChainAddresses = {
 };
 
 const BASE_ADDRESSES: ChainAddresses = {
-  v3CoreFactoryAddress: '0xC7000871C318b1f038564afd94FdcEEC4F2C8d2e',
-  NativeV3PoolDeployer: '0xA7B0d47004C4db88827B5263df8253aAc810995E',
+  v3CoreFactoryAddress: '0xB4cCd44D3bABe05DfC70b39944796d281AE905EE',
+  NativeV3PoolDeployer: '0x93c69485C572aca0467A395aF6f5c7FE49FCA83F',
   nonfungiblePositionManagerAddress:
-    '0x24400D2Ec38Db5881d03e16aEd463B1B48F7304A',
+    '0x27A60E47194e12F6bd2d0cBb7754b7739C12De20', // patched slot0.protocolFee datatype
   theGraphUrl:
-    'https://subgraph.satsuma-prod.com/6af7a179712f/native--338080/native-amm-v3-base/version/v0.0.5/api',
+    'https://gateway.thegraph.com/api/subgraphs/id/HXvzRFSnvUPQaNo99zZc7PzCyRyGb5u57H8tzL4bfAdW',
   poolInitCodeHash:
-    '0xdde0fb4ad9d3f7969c77618f019f5f8abf6ccade9fd3ee64ab6bf14968757a85',
+    '0x6f4fcfea116711b167b87469b22333669e4ceb8f39b24bef28be93f45af8b70c',
 };
 
 const BERACHAIN_ADDRESSES: ChainAddresses = {
   v3CoreFactoryAddress: '0xB4cCd44D3bABe05DfC70b39944796d281AE905EE',
   NativeV3PoolDeployer: '0x93c69485C572aca0467A395aF6f5c7FE49FCA83F',
   nonfungiblePositionManagerAddress:
-    '0x1112f006B25FFA661Bf2B0DcEcd4e4F5d0c794A3',
+    // '0x1112f006B25FFA661Bf2B0DcEcd4e4F5d0c794A3',
+    '0xADbA2DeE89196A330c1c169021e9dC6Dbcab3e8b', // patched slot0.protocolFee datatype
   theGraphUrl:
     'https://gateway.thegraph.com/api/subgraphs/id/CZTvEDJA8jmCRJ9yLjeaBpKYTUVvVpz7qwSYJeoiaEM8',
   poolInitCodeHash:
